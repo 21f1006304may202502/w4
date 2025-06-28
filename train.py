3fa0625b0774bcb54d132b289f226c19e1221a43
21f1006304may202502 <21f1006304may202502@gmail.com>
2025-06-28 17:48:42 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from pandas.plotting import parallel_coordinates
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn import metrics
+#from sklearn import metrics
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import joblib
 
 data = pd.read_csv("iris.csv")
@@ -19,7 +20,14 @@ y_test = test.species
 
 mod_dt = DecisionTreeClassifier(max_depth=3,random_state=1)
 mod_dt.fit(X_train,y_train)
-prediction = mod_dt.predict(X_test)
-print('The accuracy of the Decision Tree is',"{:.3f}".format(metrics.accuracy_score(prediction,y_test)))
+y_pred = mod_dt.predict(X_test)
+accuracy = accuracy_score(y_test,y_pred)
+precision = precision_score(y_test,y_pred)
+recall = recall_score(y_test,y_pred)
+f1 = f1_score(y_test,y_pred)
+print("accuracy:",accuracy)
+print("precision:",precision)
+print("recall:",recall)
+print("f1:",f1)
 
 joblib.dump(mod_dt, "model.joblib")
